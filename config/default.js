1b5032a10613a250bbdd1e4160fa0d4450c514f4
Sergey Yaschook <svetocvet@ukr.net>
2020-11-24 13:14:05 +0300

    Update default.js

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ config.db = {};
 config.db.name = process.env.DBNAME || 'hw3';
 config.db.host = process.env.DBHOST || 'localhost';
 config.db.port = process.env.DBPORT || 27017;
-config.db.url = process.env.DBURL || `mongodb+srv://user:user@cluster0.ojc8f.mongodb.net/hw3?retryWrites=true&w=majority`;
+config.db.url = process.env.DBURL || `yoururl`;
 config.port = process.env.PORT || 8080;
 config.secret = process.env.SECRET || 'someverysecretsecret';
 config.salt = process.env.SALT || 3;
